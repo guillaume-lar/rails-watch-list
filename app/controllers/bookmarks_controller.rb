de7dfb9f555192ac8660bc322fdb39ea42903cf3
guillaume larzilliere <guillaume3608@gmail.com>
2023-11-16 16:44:41 +0100

    p1 is done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,10 @@
+class BookmarksController < ApplicationController
+
+    def new
+        @list = List.find(params[:list_id])
+        @bookmark = Bookmark.new
+    end
+    def create
+        @bookmark = Bookmark.new
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :bookmarks, only: [:create]
+  
 end
+
+
+#  resources :gardens, only: [:index, :show] do
+#     resources :plants, only: [:create]
+#   end
+
+#   resources :plants, only: [:destroy]
